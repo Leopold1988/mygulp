512b2c9be1cea4d20578c047186a9a84560279e3
leopold1988 <542416615@qq.com>
2016-01-29 19:35:08 +0800

    1.修改gulpfile.js配置 2.添加es6转es5 3.添加提示功能 4.react依赖合并

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,10 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     autoprefixer = require('gulp-autoprefixer'),
     cssmin = require('gulp-minify-css'),
-    del = require('del');
+    clean = require('gulp-clean'),
+    plumber  = require('gulp-plumber');
 
-var open = require('gulp-open');
+var connect = require('gulp-connect');
 
 //设置目录
 var paths = {
@@ -20,28 +21,18 @@ var paths = {
   build: 'dist/'
 };
 
+
 //清理操作
-gulp.task('clean', function(cb) {
-  return del.sync([paths.build], cb);
+gulp.task('clean', function() {
+  return gulp.src('dist', {read: false}).pipe(clean());
 });
 
-//自动添加css3后缀
-// gulp.task('testAutoFx', function() {
-//   return gulp.src(paths.css + '*.css')
-//     .pipe(autoprefixer({
-//       browsers: ['last 2 versions', 'last 2 Explorer versions'],
-//       cascade: true, //是否美化属性值 默认：true 像这样：
-//       //-webkit-transform: rotate(45deg);
-//       //        transform: rotate(45deg);
-//       remove: true //是否去掉不必要的前缀 默认：true
-//     }))
-//     .pipe(gulp.dest('dist/css'));
-// });
-
-//less
-gulp.task('css', function () {
+//less&自动添加css3后缀
+gulp.task('less', function () {
   return gulp.src(paths.css + '*.less')
     .pipe(less())
+    .pipe(cssmin({compatibility: 'ie7'}))
+    .pipe(gulp.dest('dist/css'))
     .pipe(autoprefixer({
       browsers: ['last 2 versions', 'last 2 Explorer versions'],
       cascade: true, //是否美化属性值 默认：true 像这样：
@@ -49,13 +40,13 @@ gulp.task('css', function () {
       //        transform: rotate(45deg);
       remove: true //是否去掉不必要的前缀 默认：true
     }))
-    .pipe(cssmin({compatibility: 'ie7'}))
     .pipe(gulp.dest('dist/css'));
 });
 
 //js压缩
 gulp.task('jsmin', function () {
   return gulp.src(paths.scripts + '*.js')
+    .pipe(plumber())
     .pipe(uglify({
         mangle: true,//类型：Boolean 默认：true 是否修改变量名
         compress: true//类型：Boolean 默认：true 是否完全压缩
@@ -63,10 +54,34 @@ gulp.task('jsmin', function () {
     .pipe(gulp.dest('dist/js'));
 });
 
-gulp.watch(paths.css + '*', ['css']);
-gulp.watch(paths.scripts + '*', ['jsmin']);
+//copy
+// gulp.task('copy', function () {
+//   return  gulp.src(['src/**'])
+//     .pipe(gulp.dest('dist/'))
+// })
 
-gulp.task('build', ['clean', 'jsmin', 'css'], function() {
-  gulp.src(__filename)
-    .pipe(open({uri: 'localhost:3000'}));
+gulp.watch(paths.css + '*', ['less']).on('change', function(event) {
+  gulp.src(paths.css + '*')
+    .pipe(connect.reload());
+  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+});
+
+gulp.watch(paths.scripts + '*', ['jsmin']).on('change', function(event) {
+  gulp.src(paths.scripts + '*')
+    .pipe(connect.reload());
+  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+});
+
+// gulp.watch(paths.html + '*', ['jsmin']).on('change', function(event) {
+//   gulp.src(paths.html + '*')
+//     .pipe(connect.reload());
+//   console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+// });
+
+gulp.task('build', ['less'], function(){
+  // connect.server({
+  //   root: 'dist',
+  //   port: 3000,
+  //   livereload: true
+  // });
 });
